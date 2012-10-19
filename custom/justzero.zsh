f2bf49f34c82b7c4b4ea6ff1c4bb91137fda137c
justzero <gb.justzero@gmail.com>
2012-10-18 21:19:26 -0700

    ADD: user config

---
+++ custom/justzero.zsh
@@ -0,0 +1,67 @@
+# From : http://i.linuxtoy.org/docs/guide/ch30s04.html
+
+#关于历史纪录的配置 {{{
+#历史纪录条目数量
+export HISTSIZE=10000
+#注销后保存的历史纪录条目数量
+export SAVEHIST=10000
+#历史纪录文件
+#export HISTFILE=~/.zhistory
+#以附加的方式写入历史纪录
+setopt INC_APPEND_HISTORY
+#如果连续输入的命令相同，历史纪录中只保留一个
+setopt HIST_IGNORE_DUPS
+#为历史纪录中的命令添加时间戳
+setopt EXTENDED_HISTORY
+
+#启用 cd 命令的历史纪录，cd -[TAB]进入历史路径
+setopt AUTO_PUSHD
+#相同的历史路径只保留一个
+setopt PUSHD_IGNORE_DUPS
+
+#在命令前添加空格，不将此命令添加到纪录文件中
+#setopt HIST_IGNORE_SPACE
+#}}}
+
+##在命令前插入 sudo {{{
+#定义功能 
+sudo-command-line() {
+    [[ -z $BUFFER ]] && zle up-history
+    [[ $BUFFER != sudo\ * ]] && BUFFER="sudo $BUFFER"
+    zle end-of-line                 #光标移动到行末
+}
+zle -N sudo-command-line
+#定义快捷键为： [Esc] [Esc]
+bindkey "\e\e" sudo-command-line
+#}}}
+
+#命令别名 {{{
+alias -g cp='cp -i'
+alias -g mv='mv -i'
+alias -g rm='rm -i'
+alias -g ls='ls -F --color=auto'
+alias -g ll='ls -l'
+alias -g grep='grep --color=auto'
+
+#压缩
+alias -s gz='tar -xzvf'
+alias -s bz2='tar -xjvf'
+
+# command L equivalent to command |less
+alias -g L='|less'
+
+#[Esc][h] man 当前命令时，显示简短说明 
+alias run-help >&/dev/null && unalias run-help
+autoload run-help
+
+#历史命令 top10
+alias top10='print -l  ${(o)history%% *} | uniq -c | sort -nr | head -n 10'
+#}}}
+
+#路径别名 {{{
+#进入相应的路径时只要 cd ~xxx
+hash -d E="/etc/env.d"
+hash -d C="/etc/conf.d"
+hash -d I="/etc/rc.d"
+hash -d X="/etc/X11"
+#}}}
